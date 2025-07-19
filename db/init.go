5e4362ab6332fd72ff096c5a1036f6dde46ba8dd
xinyi-chong <xinyi.chong.dev@gmail.com>
2025-07-19 17:13:09 +0800

    feat(db): add postgres connection

---
+++ db/init.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"time"
+)
+
+type Config struct {
+	Host            string        `mapstructure:"host" validate:"required"`
+	Port            int           `mapstructure:"port" validate:"required"`
+	User            string        `mapstructure:"user" validate:"required"`
+	Password        string        `mapstructure:"password" validate:"required"`
+	DB              string        `mapstructure:"db" validate:"required"`
+	SSLMode         string        `mapstructure:"sslmode" validate:"required,oneof=disable require verify-ca verify-full"`
+	MaxIdleConns    int           `mapstructure:"max_idle_conns" validate:"omitempty,min=1"`
+	MaxOpenConns    int           `mapstructure:"max_open_conns" validate:"omitempty,min=1"`
+	ConnMaxLifetime time.Duration `mapstructure:"conn_max_lifetime" validate:"omitempty,min=1m"`
+}
+
+func Init(cfg Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host,
+		cfg.Port,
+		cfg.User,
+		cfg.Password,
+		cfg.DB,
+		cfg.SSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
+		PrepareStmt: true,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	if cfg.MaxIdleConns <= 0 {
+		cfg.MaxIdleConns = 5
+	}
+	if cfg.MaxOpenConns <= 0 {
+		cfg.MaxOpenConns = 25
+	}
+	if cfg.ConnMaxLifetime <= 0 {
+		cfg.ConnMaxLifetime = 30 * time.Minute
+	}
+
+	sqlDB.SetMaxIdleConns(cfg.MaxIdleConns)
+	sqlDB.SetMaxOpenConns(cfg.MaxOpenConns)
+	sqlDB.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+
+	return db, nil
+}
