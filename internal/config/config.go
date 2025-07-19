03d1239185856fa4ffb4a5514cea0f69508af5e6
xinyi-chong <xinyi.chong.dev@gmail.com>
2025-07-19 17:09:48 +0800

    feat(config): load and validate config via viper and validator

---
+++ internal/config/config.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	"auth-service/db"
+	"auth-service/pkg/logger"
+	redisclient "auth-service/pkg/redis"
+	"errors"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"strings"
+	"time"
+)
+
+type Config struct {
+	Server struct {
+		Host string `mapstructure:"host"`
+		Port string `mapstructure:"port" validate:"required"`
+	} `mapstructure:"server"`
+
+	Postgres struct {
+		db.Config `mapstructure:",squash"`
+	} `mapstructure:"postgres"`
+
+	Redis struct {
+		redisclient.Config `mapstructure:",squash"`
+	} `mapstructure:"redis"`
+
+	JWT struct {
+		AccessDuration  time.Duration `mapstructure:"access_duration"`
+		RefreshDuration time.Duration `mapstructure:"refresh_duration"`
+	} `mapstructure:"jwt"`
+}
+
+func (c *Config) Validate() error {
+	validate := validator.New()
+	err := validate.Struct(c)
+	if err != nil {
+		var validationErrors validator.ValidationErrors
+		if errors.As(err, &validationErrors) {
+			fmt.Println("Validation errors:")
+			for _, e := range validationErrors {
+				fmt.Printf("Field: %s, Tag: %s, Value: %v\n", e.Field(), e.Tag(), e.Value())
+			}
+		}
+		return err
+	}
+	return nil
+}
+
+func Load() (*Config, error) {
+	v := setupViper()
+
+	if err := v.ReadInConfig(); err != nil {
+		logger.Error("Load: ReadInConfig: ", zap.Error(err))
+		return nil, err
+	}
+
+	var config Config
+	if err := v.Unmarshal(&config); err != nil {
+		logger.Error("Load: Unmarshal config: ", zap.Error(err))
+		return nil, err
+	}
+	logger.Debug("Load: config: ", zap.Any("config", config))
+
+	if err := config.Validate(); err != nil {
+		logger.Error("Load: Validate: ", zap.Error(err))
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func setupViper() *viper.Viper {
+	v := viper.New()
+
+	v.SetConfigName("auth-service")
+	v.SetConfigType("yaml")
+	v.AddConfigPath("/app/configs")
+	v.AddConfigPath(".") // For local development
+
+	v.AutomaticEnv()
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	return v
+}
