b656ce3c2654efb8b8cb2b81c4b232192d6f3306
xinyi-chong <xinyi.chong.dev@gmail.com>
2025-08-22 00:24:03 +0800

    feat(utils): add password and email utility functions

---
+++ internal/shared/utils/utils.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"regexp"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func CheckPassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
+
+func IsValidEmail(email string) bool {
+	var emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(email)
+}
